14535ac2157da43cb29e5bb1d32ce764b14b8f32
Amit Prasad <mail@amitprasad.dev>
2022-11-04 18:03:36 -0400

    Tracking V2 changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,14 @@
 /**
  * Copyright (c) 2022-present, Amit Prasad
  * All rights reserved.
- * 
+ *
  * This source code is licensed under the license found in the
  * LICENSE file in the root directory of this source tree.
- */ 
-
+ */
 pub mod beacon;
 pub mod msg;
-pub mod provide;
 pub mod proof;
-pub use beacon::{EntropyCallbackMsg, EntropyRequest, calculate_gas_cost};
+pub mod provide;
+pub use beacon::{EntropyCallbackMsg, EntropyRequest};
+pub use msg::QueryMsg as BeaconQueryMsg;
 pub use provide::{BeaconConfigQuery, BeaconConfigResponse};
-pub use msg::{QueryMsg as BeaconQueryMsg};
